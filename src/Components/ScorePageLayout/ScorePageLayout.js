e71811aa8029b5dc87919231095ea07fdbcbe557
Tobe <mac@Tobes-MacBook-Pro.local>
2022-12-30 11:11:24 +0100

    dealt with  calender // cleared react errors

--- src/Components/ScorePageLayout/ScorePageLayout.js
+++ src/Components/ScorePageLayout/ScorePageLayout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../Layout/Layout";
 import ScoreAdSection from "./ScoreAdSection";
 import ScoreMobileAdSection from "./ScoreMobileAdSection";
